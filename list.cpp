c06d93d9e0553f797871a010247e764ba74ceac9
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-06-06 23:45:14 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -186,3 +186,105 @@ void List :: print_list()
     }
 }
 
+
+int list() {
+
+    // Создание экземпляра списка
+    List list;
+    std::cout << std::endl;
+    // Исходное состояние
+    list.print_list();
+
+    std::string option;
+    std::cout << "\n Choose option: \n a1 - add an element to the beginning \n a2 - add an element to the end \n b - add an element to a curtain position \n c - delete element with a curtain value \n d - delete element with a curtain position \n e - search for element with a curtain value \n f - search for element with a curtain position \n g - print all elements \n s - stop " << std::endl;
+    while (option != "s" && std::cin.good()) {
+        std::cin >> option;
+        int value;
+        int position;
+        if (option == "a1") {
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            list.add_first(value);
+            std::cout << "[ " << value << " ] added to the beginning" << std::endl;
+            std::cout << "Choose option. " << std::endl;
+        }
+        else if (option == "a2") {
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            list.add_last(value);
+            std::cout << "[ " << value << " ] added to the end" << std::endl;
+            std::cout << "Choose option. " << std::endl;
+        } else if (option == "b") {
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            std::cout << "Enter position: ";
+            std::cin >> position;
+            if (list.search_position(position-1) == nullptr)
+            {
+                std::cout << "Impossible. " << std::endl;
+            }
+            else
+            {
+                list.add_position(value, position);
+                std::cout << "[ " << value << " ] added to position " << position << std::endl;
+            }
+            std::cout << "Choose option. " << std::endl;
+        } else if (option == "c") {
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            if (list.search_value(value) == nullptr)
+            {
+                std::cout << "No element with this value." << std::endl;
+            }
+            else
+            {
+                list.delete_value(value);
+                std::cout << "Element is deleted." << std::endl;
+            }
+            std::cout << "Choose option. " << std::endl;
+        } else if (option == "d") {
+            std::cout << "Enter position: ";
+            std::cin >> position;
+            if (list.search_position(position) == nullptr)
+            {
+                std::cout << "No element in this position." << std::endl;
+            }
+            else
+            {
+                list.delete_position(position);
+                std::cout << "Element is deleted." << std::endl;
+            }
+            std::cout << "Choose option. " << std::endl;
+        } else if (option == "e") {
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            if (list.search_value(value) == nullptr)
+            {
+                std::cout << "No element with this value." << std::endl;
+            }
+            else
+            {
+                std::cout << "[ " << list.search_value(value)->data << " ] is found " << std::endl;
+            }
+            std::cout << "Choose option. " << std::endl;
+        } else if (option == "f") {
+            std::cout << "Enter position: ";
+            std::cin >> position;
+            if (list.search_position(position) == nullptr)
+            {
+                std::cout << "No element in this position." << std::endl;
+            }
+            else
+            {
+                std::cout << "[ " << list.search_position(position)->data << " ] is found in position " << position << std::endl;
+                std::cout << "Choose option. " << std::endl;
+            }
+        } else if (option == "g") { list.print_list(); std::cout << "Choose option. " << std::endl;}
+        else if (option == "s") { std::cout << std::endl; }
+        else { std::cout << "Incorrect option" << std::endl; }
+
+    }
+
+
+    return 0;
+}
