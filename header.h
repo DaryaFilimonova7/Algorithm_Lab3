0bad040ca0ffd308d400a116c3491253ae966e07
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-06-06 23:45:40 +0300

    Update header.h

--- header.h
+++ header.h
@@ -34,3 +34,35 @@ public:
     void print_list();
 };
 
+
+
+class Node_char
+{
+public:
+    char data;
+    class Node_char* next;
+};
+
+class List_char
+{
+    // Указатели на первый и последний эл-ты списка
+    Node_char *first;
+    Node_char *last;
+
+public:
+    // Инициализация списка по умолчанию
+    List_char()
+    {
+        first = nullptr;
+        last = nullptr;
+    }
+
+    Node_char* add_first(char);
+    Node_char* add_last(char);
+    Node_char* add_position(char,int);
+    void delete_value(char);
+    void delete_position(int);
+    Node_char* search_value(char);
+    Node_char* search_position(int);
+    void print_list();
+};
